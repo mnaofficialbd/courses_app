972cf593054bfa83a7674dea7bef7331bff6d39b
mnaofficialbd <mna.official92@gmail.com>
2024-01-09 19:01:01 +0600

    simple app create with nexr.js

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,13 +1,14 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 const CourseCard = ({ course }) => {
   return (
     <div>
       <h2>{course.title}</h2>
       <p>{course.description}</p>
-      <Link href={`/courses/${course.slug}`}>
+      <a href={`/courses/${course.slug}`}>View Details</a>
+      {/* <Link href={`/courses/${course.slug}`}>
         <a>View Details</a>
-      </Link>
+      </Link> */}
     </div>
   );
 };
